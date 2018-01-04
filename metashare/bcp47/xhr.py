d48e66d5252f85fc6485a04053454c91377b9e1c
MiltosD <mdeligiannis@outlook.com>
2018-01-04 09:28:55 +0200

    remove prints

--- metashare/bcp47/xhr.py
+++ metashare/bcp47/xhr.py
@@ -2,10 +2,12 @@ from django.http import HttpResponse
 
 from metashare.bcp47 import iana
 
+
 def update_lang_variants(request):
     lang = request.POST.get('lang')
     variants = iana.get_variants_by_language(lang)
     return HttpResponse("//".join(variants))
+
 
 def update_lang_variants_with_script(request):
     lang = request.POST.get('lang')
@@ -13,9 +15,8 @@ def update_lang_variants_with_script(request):
     variants = iana.get_variants_by_script(script, lang)
     return HttpResponse("//".join(variants))
 
+
 def update_var_variants(request):
-    print request.POST
     variant = request.POST.get('variant')
     variants = iana.get_variants_by_variant(variant)
-    print variant, variants
     return HttpResponse("//".join(variants))
