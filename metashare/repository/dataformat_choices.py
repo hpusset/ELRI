db636e47d3cfc30ce93af11345151d7c4ac6bbfe
MiltosD <mdeligiannis@outlook.com>
2018-03-19 11:55:47 +0200

    complete dataformats

--- metashare/repository/dataformat_choices.py
+++ metashare/repository/dataformat_choices.py
@@ -20,12 +20,16 @@ MIMETYPELABEL_TO_MIMETYPEVALUE[u'Term Base eXchange'] = u"application/x-tbx"
 MIMETYPELABEL_TO_MIMETYPEVALUE[u'JSON'] = u"application/json"
 MIMETYPELABEL_TO_MIMETYPEVALUE[u'SGML'] = u"text/sgml"
 MIMETYPELABEL_TO_MIMETYPEVALUE[u'MS-Access database'] = u"application/x-msaccess"
+MIMETYPELABEL_TO_MIMETYPEVALUE[u'MS-Excel xls'] = u"application/vnd.ms-excel"
+MIMETYPELABEL_TO_MIMETYPEVALUE[u'MS-Excel xlsx'] = u"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+MIMETYPELABEL_TO_MIMETYPEVALUE[u'MS-Word docx'] = u"application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+MIMETYPELABEL_TO_MIMETYPEVALUE[u'MS-Word doc'] = u"application/msword"
 MIMETYPELABEL_TO_MIMETYPEVALUE[u'TM format of the SDL alignment tool'] = u'application/x-SDL-TM'
 MIMETYPELABEL_TO_MIMETYPEVALUE[u'RDF'] = u"application/rdf+xml"
 MIMETYPELABEL_TO_MIMETYPEVALUE[u'XHTML'] = u"application/xhtml+xml"
 MIMETYPELABEL_TO_MIMETYPEVALUE[u'RTF'] = u"application/rtf"
 MIMETYPELABEL_TO_MIMETYPEVALUE[u'other'] = u"other"
-MIMETYPELABEL_TO_MIMETYPEVALUE[u'plain text'] = u"text/plain"
+MIMETYPELABEL_TO_MIMETYPEVALUE[u'Plain text'] = u"text/plain"
 MIMETYPELABEL_TO_MIMETYPEVALUE[u'XCES'] = u"application/x-xces+xml"
 
 MIMETYPEVALUE_TO_MIMETYPELABEL = {}
@@ -56,16 +60,28 @@ MIMETYPEVALUE_TO_MIMETYPELABEL[u'application/vnd.ms-excel'] = u"MS-Excel xls"
 MIMETYPEVALUE_TO_MIMETYPELABEL[u'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'] = u"MS-Excel xlsx"
 
 TEXTFORMATINFOTYPE_DATAFORMAT_CHOICES = _make_choices_from_list_alt([
-u'text/plain',u'application/vnd.xmi+xml',
-u'application/xml', u'application/json', u'application/x-tmx+xml',
-u'application/x-xces+xml', u'application/tei+xml',u'application/rdf+xml',
-u'application/x-tbx',
-u'application/xhtml+xml', u'text/sgml',
-u'text/html', u'application/x-tex',
-u'application/rtf', u'application/x-latex',
-u'text/csv',u'text/tab-separated-values',
-u'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-u'application/msword',
-u'application/pdf',u'application/x-msaccess', u'application/x-SDL-TM',
-u'application/vnd.ms-excel',
-u'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', u'other'],MIMETYPEVALUE_TO_MIMETYPELABEL)
+    u'text/plain',
+    u'application/vnd.xmi+xml',
+    u'application/xml',
+    u'application/json',
+    u'application/x-tmx+xml',
+    u'application/x-xces+xml',
+    u'application/tei+xml',
+    u'application/rdf+xml',
+    u'application/x-tbx',
+    u'application/xhtml+xml',
+    u'text/sgml',
+    u'text/html',
+    u'application/x-tex',
+    u'application/rtf',
+    u'application/x-latex',
+    u'text/csv',
+    u'text/tab-separated-values',
+    u'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+    u'application/msword',
+    u'application/pdf',
+    u'application/x-msaccess',
+    u'application/x-SDL-TM',
+    u'application/vnd.ms-excel',
+    u'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    u'other'], MIMETYPEVALUE_TO_MIMETYPELABEL)
