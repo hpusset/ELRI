055bf7f92157018d5047af159c870fc32e17ad6d
MiltosD <mdeligiannis@outlook.com>
2018-03-19 08:51:08 +0200

    change list values

--- project_management/models.py
+++ project_management/models.py
@@ -4,11 +4,11 @@ from metashare.repository.models import resourceInfoType_model
 from metashare.repository.supermodel import _make_choices_from_list
 
 DELIVERABLES = _make_choices_from_list(
-    ["CEF-ELRC", "ELRC+ LOT2",
-     "LOT3 D2.1", "LOT3 D2.2",
-     "LOT3 D2.3", "LOT3 D2.4",
-     "LOT3 D3.1", "LOT3 D3.2",
-     "LOT3 D3.3"
+    ["CEF-ELRC", "ELRC Network",
+     "ELRC Data D2.1", "ELRC Data D2.2",
+     "ELRC Data D2.3", "ELRC Data D2.4",
+     "ELRC Data D3.1", "ELRC Data D3.2",
+     "ELRC Data D3.3"
      ]
 )
 
@@ -26,17 +26,24 @@ class ManagementObject(models.Model):
         choices=sorted(DELIVERABLES['choices']),
         max_length=10, blank=True, null=True)
 
+    is_processed_version = models.BooleanField(verbose_name="Is Processed Version", default=False, editable=False)
+
     rejected = models.BooleanField(verbose_name="Rejected", default=False)
 
     rejection_reason = models.TextField(max_length=1000, blank=True, null=True)
 
     unique_together = ("rejected", "rejection_reason")
 
+    def _set_is_processed_version(self):
+        relations = [relation.relationType.startswith('is') for relation in self.resource.relationinfotype_model_set.all()]
+        self.is_processed_version = any(relations)
+
     def __unicode__(self):
         _unicode = u'{} (id: "{}")'.format(self.resource, self.id)
         return _unicode
 
     def save(self, *args, **kwargs):
+        self._set_is_processed_version()
         if not self.resource:
             self.resource = self.get_related_resource().__unicode__()
         if self.rejected and not self.rejection_reason:
