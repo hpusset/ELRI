baaf79139ec88b41cf454fa13f74613330566ff2
MiltosD <mdeligiannis@outlook.com>
2018-04-02 11:06:22 +0300

    my processes

--- metashare/processing/urls.py
+++ metashare/processing/urls.py
@@ -4,5 +4,6 @@ urlpatterns = patterns('metashare.processing.views',
                        url(r'^$', 'services'),
                        url(r'^process/(?P<resource_id>\w+)/$', 'process_repo_resource'),
                        url(r'^data-transaction/$', 'get_data'),
-                       url(r'^download/(?P<processing_id>\w+)/$', 'download_processed_data')
+                       url(r'^download/(?P<processing_id>\w+)/$', 'download_processed_data'),
+                       url(r'^my/$', 'my_processings')
                        )
