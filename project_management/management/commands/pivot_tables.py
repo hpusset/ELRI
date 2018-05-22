75a2c4e43f1fdf4e8ee51051f513f3c86484d1ae
MiltosD <mdeligiannis@outlook.com>
2018-05-22 15:57:22 +0300

    add pivot report

--- project_management/management/commands/pivot_tables.py
+++ project_management/management/commands/pivot_tables.py
@@ -5,4 +5,4 @@ from metashare.report_utils.pivot_tables import create_pivot_report
 class Command(BaseCommand):
     def handle(self, *args, **options):
         self.stdout.write("Creating CEF Digital pivot report\n")
-        self.stdout.write(str(create_pivot_report))
+        self.stdout.write(str(create_pivot_report()))
