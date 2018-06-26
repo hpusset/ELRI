1952c6004b19546127070aa80addbe1aaafaa50f
MiltosD <mdeligiannis@outlook.com>
2018-06-26 11:41:43 +0300

    add haystack filters

---
+++ metashare/repository/api/haystack_filters.py
@@ -0,0 +1,9 @@
+haystack_filters = {"language": "languageName",
+                    "textGenre": "textTextGenre",
+                    "textType": "textTextType",
+                    "conditionOfUse": "restrictionsOfUse",
+                    "linguality": "lingualityType",
+                    "multilinguality": "multilingualityType",
+                    "mimetype": "dataFormat",
+                    "dsi": "appropriatenessForDSI",
+                    "status": "publicationStatus"}
