762baff937d42407a09881b1a4697a05f7b00b1d
MiltosD <mdeligiannis@outlook.com>
2018-06-26 14:19:17 +0300

    use custom API key authentication

--- metashare/repository/api/auth.py
+++ metashare/repository/api/auth.py
@@ -1,24 +1,32 @@
-from tastypie.authorization import Authorization
-from tastypie.exceptions import Unauthorized
+from metashare.settings import REST_API_KEY
+from tastypie.authentication import Authentication
+from tastypie.http import HttpUnauthorized
 
 
-class ApiDjangoAuthorization(Authorization):
+class RepositoryApiKeyAuthentication(Authentication):
+    def _unauthorized(self):
+        return HttpUnauthorized()
 
-    READ_PERM_CODE = 'access'  # matching respective Permission.codename
+    def is_authenticated(self, request, **kwargs):
 
-    def create_list(self, object_list, bundle):
-        pass
+        # superusers and anyone with permission does not need to define an API key
+        if request.user.is_superuser or request.user.has_perm('auth.access_api'):
+            return True
+        # Check for api key if the user is not logged in the repository (e.g. cURL, etc)
+        # First check if the key is in the "auth" param
+        elif request.GET.get('auth'):
+            api_key = request.GET.get('auth').strip()
+        # Next check if the key is in the request header
+        elif request.META.get('HTTP_AUTHORIZATION'):
+            api_key = request.META['HTTP_AUTHORIZATION'].strip()
+        # if all above fails, the user is unauthorized
+        else:
+            return self._unauthorized()
 
-    def read_list(self, object_list, bundle):
-        # now we check here for specific permission
-        if not bundle.request.user.has_perm('auth.access_api'):
-            raise Unauthorized("You are not allowed to access that resource.")
-        result = super(ApiDjangoAuthorization, self).read_list(object_list, bundle)
-        return result
+        key_auth_check = self.get_key(api_key)
+        return key_auth_check
 
-    def read_detail(self, object_list, bundle):
-        # now we check here for specific permission
-        if not bundle.request.user.has_perm('auth.access_api'):
-            raise Unauthorized("You are not allowed to access that resource.")
-        result = super(ApiDjangoAuthorization, self).read_detail(object_list, bundle)
-        return result
+    def get_key(self, api_key):
+        if api_key != REST_API_KEY.strip():
+            return self._unauthorized()
+        return True
