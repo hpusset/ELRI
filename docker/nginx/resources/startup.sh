3d173ea6c7cf87f66dae2f2e35e9842b2987e8df
jcorteslinkare <jcortes@linkare.com>
2018-12-05 15:13:33 +0000

    Initial structure commit with instructions to startup environment

---
+++ docker/nginx/resources/startup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source /env_secrets_expand.sh
+
+cmd="dockerize"
+
+if [ "$ELRI_NODES" != "" ]; then
+    for node in ${ELRI_NODES}; do
+		cmd="$cmd -wait http://${node}/elri -wait-http-header X-Forwarded-Proto:http"
+	done
+fi
+
+cmd="$cmd -template /etc/nginx/conf.d/vhost.tmpl:/etc/nginx/conf.d/vhost.conf"
+cmd="$cmd -timeout 240s nginx"
+
+$cmd
