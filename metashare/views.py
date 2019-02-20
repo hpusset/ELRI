92d60759baeeb6a8c9baa6396cb700d48c183b35
emartinez <emartinez@vicomtech.org>
2019-02-20 17:56:35 +0100

    Custom login error messages. #81

--- metashare/views.py
+++ metashare/views.py
@@ -1,18 +1,27 @@
 import logging
 from django.utils import translation
+from django.utils.translation import ugettext as _
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from metashare.repository.models import resourceInfoType_model
 from metashare.settings import LOG_HANDLER, COUNTRY, LANGUAGE_CODE
 from metashare.storage.models import PUBLISHED
 
-
-# Setup logging support.
+# Setup logging support,
 LOGGER = logging.getLogger(__name__)
 LOGGER.addHandler(LOG_HANDLER)
 
 translation.activate(LANGUAGE_CODE)
+
+#Custom authentication error messages: 
+class ELRIAuthenticationForm(AuthenticationForm):
+	error_messages = {
+		'invalid_login': _("Please enter a correct %(username)s and password, Note that both "
+            "fields may be case-sensitive."),
+		'inactive': _("This account has not been activated yet. Please contact us if there is any issue with an existing account."), 
+		}
 
 def frontpage(request):
     """Renders the front page view."""
@@ -30,12 +39,12 @@ def frontpage(request):
       context_instance=RequestContext(request))
 
 
-def login(request, template_name):
+def login(request, template_name, redirect_field_name=None, authentication_form=ELRIAuthenticationForm ):
     """Renders login view by connecting to django.contrib.auth.views."""
     LOGGER.info(u'Rendering login view for user "{0}".'.format(
       request.user.username or "Anonymous"))
 
-    return auth_views.login(request, template_name)
+    return auth_views.login(request, template_name, redirect_field_name, authentication_form)
 
 
 def logout(request):
