7ed0118676e3f45be780a2ccb1adfcdaf448116e
emartinez <emartinez@vicomtech.org>
2019-02-28 17:27:26 +0100

    minor changes temporary ui

--- metashare/accounts/forms.py
+++ metashare/accounts/forms.py
@@ -85,9 +85,9 @@ class RegistrationRequestForm(Form):
                             % _("Organization address")))
     organization_phone_number = forms.CharField(
             UserProfile._meta.get_field('affiliation_phone_number').max_length,
-            label=mark_safe(u"%s"% _("Organization phone number")))
+            label=mark_safe(u"%s<span style='color:red'>*</span>" % _("Organization phone number")))
     position = forms.CharField(UserProfile._meta.get_field('position').max_length,
-                               label=mark_safe(u"%s"% _("Position in the organization")))
+                               label=mark_safe(u"%s<span style='color:red'>*</span>" % _("Position in the organization")))
                                
     #Removing user phone number for now: user email and organisation phone number should be sufficient
     #phone_number = forms.CharField(UserProfile._meta.get_field('phone_number').max_length,
