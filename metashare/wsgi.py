c7f7017ff9eb62c9742e3492e1cbe3e8e6a352b4
emartinez <emartinez@vicomtech.org>
2019-07-22 14:07:29 +0200

    update wsgi.py for new docker image

--- metashare/wsgi.py
+++ metashare/wsgi.py
@@ -1,6 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-#os.environ['DJANGO_SETTINGS_MODULE'] = 'metashare.settings'
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'metashare.settings')
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'metashare.settings'
 application = get_wsgi_application()
